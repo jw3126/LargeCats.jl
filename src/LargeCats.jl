848dab4ec10e4c474ef9fb2012ecae3cdc1b9554
Jan Weidner <jw3126@gmail.com>
2021-01-05 11:38:31 +0100

    init

--- src/LargeCats.jl
+++ src/LargeCats.jl
@@ -1,5 +1,80 @@
 module LargeCats
+export largecat
 
-# Write your package code here.
-
+function similar_ntuple(f, t::NTuple{N, <: Any}) where {N}
+    ntuple(f, N)
 end
+
+function ifelse_dims(f_in_dims, f_else, t1::Tuple, t2::Tuple, dims)
+    map(similar_ntuple(identity, t1), t1, t2) do i, x1, x2
+        if i in dims
+            f_in_dims(x1,x2)
+        else
+            f_else(x1,x2)
+        end::Int
+    end
+end
+
+function ifelse_dims(f_in_dims, f_else, c1::CartesianIndex, c2::CartesianIndex, dims)
+    t = ifelse_dims(f_in_dims, f_else, Tuple(c1), Tuple(c2), dims)
+    CartesianIndex(t)
+end
+
+firstarg(x1,x2) = x1
+secondarg(x1,x2) = x2
+samearg(x1, x2) = (@assert x1 === x2; x1)
+@noinline function largecat!(out::AbstractArray{N}, itr; dims) where {N}
+    Base.require_one_based_indexing(out)
+    I_start = first(CartesianIndices(out))
+    I_outsize = CartesianIndex(size(out))
+    I_stop  = ifelse_dims((i1, i2) -> i1-1, secondarg, I_start, I_outsize, dims)
+    for x in itr
+        Base.require_one_based_indexing(x)
+        I_size = CartesianIndex(size(x))
+        I_stop = ifelse_dims(+, firstarg, I_stop, I_size, dims)
+        # @show I_size
+        # @show I_start
+        # @show I_stop
+        out[I_start:I_stop] .= x
+        I_start = ifelse_dims(+, firstarg, I_start, I_size, dims)
+    end
+    out
+end
+
+function largecat(itr; dims)
+    T = largecat_eltype(itr)
+    outsize = largecat_outsize(itr, dims=dims)
+    out = zeros(T, outsize)
+    largecat!(out, itr, dims=dims)
+end
+
+function largecat_outsize(itr; dims)
+    dims1 = size(first(itr))
+    out_size = map(similar_ntuple(identity, dims1), dims1) do i, d
+        if i in dims
+            0
+        else
+            d
+        end
+    end
+    for x in itr
+        Base.require_one_based_indexing(x)
+        out_size = ifelse_dims(+, samearg, out_size, size(x), dims)
+    end
+    return out_size
+end
+
+function largecat_eltype(itr)
+    T = eltype(eltype(itr))
+    if Base.isconcretetype(T)
+        return T
+    else
+        compute_largecat_eltype(itr)
+    end
+end
+
+function compute_largecat_eltype(itr)
+    mapreduce(eltype, promote_type, itr)
+end
+
+end #module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,53 @@
 using LargeCats
+const LC = LargeCats
 using Test
 
-@testset "LargeCats.jl" begin
-    # Write your tests here.
+@testset "internals" begin
+    arr1 = randn(1,2,3)
+    arr2 = randn(1,2,4)
+
+    dims= (3,2)
+    @test (1,4,7) === @inferred(LC.largecat_outsize([arr1, arr2], dims=(3,2)))
+    @test Float64 === @inferred(LC.largecat_eltype([arr1, arr2]))
+
+    @test (2,)    === @inferred(LC.largecat_outsize([[1,2]], dims=1))
+    @test Int     === @inferred(LC.largecat_eltype([[1,2], [3,4]]))
+    @test Int     === @inferred(LC.largecat_eltype(([1],[2])))
+    @test Float64 === @inferred(LC.largecat_eltype(([1],[2.2])))
+
+end
+
+@testset "against cat" begin
+    arr1 = randn(1,2,3)
+    arr2 = randn(1,2,4)
+    arrs = [arr1, arr2]
+    dims = (2,3)
+    @test @inferred(largecat(arrs, dims=dims)) == cat(arrs..., dims=dims)
+end
+
+@testset "largecat perf " begin
+    function printrow(n, rel_time, tot_time, mem, outsize)
+        println("|",
+          rpad(string(n   ), 10), "|",
+          rpad(string(rel_time), 15), "|",
+          rpad(string(tot_time), 15), "|",
+          rpad(string(mem), 15), "|",
+          rpad(string(outsize), 15), "|",
+        )
+    end
+    println("Performance counters")
+    printrow("n arrays", "time/length/ns", "total time/s", "allocs/bytes", "outsize/bytes")
+
+    for n in Int[1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6]
+        arrs = map(1:Int(n)) do i
+            rand(Float32, rand(1:3), 1)
+        end
+        largecat(arrs[1:1],dims=(1,)) # warmup
+        stats = @timed largecat(arrs,dims=(1,))
+        outsize = Base.summarysize(stats.value)
+        rel_time = round(1e9*stats.time/length(stats.value), digits=3)
+        printrow(n,rel_time , stats.time, stats.bytes, outsize)
+        # @info "$(stats.time)s $(stats.bytes) bytes"
+        @test outsize < stats.bytes < outsize + 1000
+    end
 end
